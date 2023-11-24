815c78cefd3b3482871e7e54b3353fb8cc330c3f
Leslie M <lesliemariemac@gmail.com>
2023-11-23 22:25:22 -0500

    Added two functions with parameters

---
+++ script.js
@@ -0,0 +1,18 @@
+//Function with no parameters
+var startMyDay = function () {
+    return "Time to walk!";
+};
+console.log(startMyDay());
+
+
+
+//Function with 1 paramater
+var favoriteCookie = function (cookie) {
+   return `My favorite cookie is ${cookie}.`;
+};
+//console.log(favoriteCookie());
+console.log(favoriteCookie("peanut butter")); // My favorite cookie is peanut butter.
+
+
+
+
